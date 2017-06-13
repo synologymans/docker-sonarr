6fa4095cd99605cdc8e1a877980708b094f8be43
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-06-13 11:38:03 +0200

    new mqtt location

--- mosquittonotifier.sh
+++ mosquittonotifier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/mosquitto_pub -h 10.0.1.52 -t "downloads/1" -m "Download complete: $sonarr_episodefile_scenename"
+/usr/bin/mosquitto_pub -h 10.0.1.11 -t "/downloads" -m "Download complete: $sonarr_episodefile_scenename"
